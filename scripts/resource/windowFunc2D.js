094aad2df2d6c24f8736ff8c6cbf723711b2f0c2
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-11-25 16:23:27 +0700

    Need for custom kernel for Void and Cluster method

---
+++ scripts/resource/windowFunc2D.js
@@ -0,0 +1,62 @@
+const $windowFunc2D = (function () {
+  const _getPreset = (preset) => {
+    const presets = {
+      rect: "1",
+      ramp: "(y * width + x) / (width * height)",
+      chebyshev:
+        "1 -\n" +
+        "Math.max(\n" +
+        "Math.abs(x - width / 2),\n" +
+        "Math.abs(y - height / 2)\n" +
+        ") /\n" +
+        "Math.max(width / 2, height / 2)\n",
+      welch: "1 - ((n - N / 2) / (N / 2)) ** 2",
+      cosine:
+        "Math.cos((Math.PI * (x - width / 2)) / width) * Math.cos((Math.PI * (y - height / 2)) / height)",
+      gaussian: "Math.exp(-((x - width / 2) ** 2 + (y - height / 2) ** 2) / (2 * 1.5 ** 2))",
+    };
+
+    const choosenPreset = presets[preset];
+
+    if (choosenPreset) {
+      return choosenPreset;
+    } else {
+      return false;
+    }
+  };
+
+  const windowFuncLUT = new Map();
+
+  const _getWindowFunctionLUT = (width, height, equation) => {
+    if (!Number.isInteger(width)) throw new Error("Width must be an integer");
+    if (!Number.isInteger(height)) throw new Error("Height must be an integer");
+
+    const sqSz = width * height;
+    if (equation == null) throw new Error("Invalid equation input: " + equation);
+
+    const trimmed = equation.replace(/\s/g, "");
+    const key = width + "x" + height + " | " + trimmed;
+
+    if (!windowFuncLUT.has(key)) {
+      const array = new Float64Array(sqSz).fill(1);
+      const cp = new Function("x", "y", "width", "height", "return " + trimmed);
+
+      for (let y = 0; y < height; y++) {
+        const yOffs = y * width;
+
+        for (let x = 0; x < width; x++) {
+          array[yOffs + x] *= cp(x, y, width, height);
+        }
+      }
+
+      windowFuncLUT.set(key, array);
+    }
+
+    return windowFuncLUT.get(key);
+  };
+
+  return {
+    getPreset: _getPreset,
+    getWindowFunctionLUT: _getWindowFunctionLUT,
+  };
+})();
